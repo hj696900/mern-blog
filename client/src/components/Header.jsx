ff89fa29d9092c97a63cb1fbcf7600dc5463b7d9
harsh0559.be21@chitkara.edu.in <hj696900@gmail.com>
2024-02-15 15:59:13 +0530

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
         <Link to="/" className="self-center whitespace-nowrap text-sm
         sm:text-xl font-semibold dark:text-white">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500
-            via-purple-500 to to-pink-500 rounded-lg text-white">Harsh's</span>
+            via-purple-500 to to-pink-500 rounded-lg text-white">Harsh</span>
             Blog
         </Link>
         <form>
@@ -32,7 +32,7 @@ export default function Header() {
             <FaMoon />
           </Button>
         <Link to='/sign-in'>
-        <Button gradientDuoTone='purpleToBlue'>
+        <Button gradientDuoTone='purpleToBlue' outline>
               Sign In
         </Button>
         </Link>
@@ -43,7 +43,7 @@ export default function Header() {
         <Navbar.Link active={path === "/"} as={'div'}>
           <Link to='/'>Home</Link>
         </Navbar.Link>
-          
+
         <Navbar.Link active={path === "/about"} as={'div'}>
           <Link to='/about'>About</Link>
           </Navbar.Link>
